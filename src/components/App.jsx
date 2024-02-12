b68ca97f71e95f236692330ca8efb0e233a0b622
DmytroKorolyov <148568611+DmytroKorolyov@users.noreply.github.com>
2024-02-12 21:03:17 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import { ReactDOM } from "react";
+import Profile from "../components/Profile/Profile.jsx";
+import UserData from '../data/user.json'
+import Statistics from "../components/Statistics/Statistics.jsx";
+import UserStat from '../data/data.json'
+
+
+
+
 export const App = () => {
-  return (
+  const { username, tag, location, avatar, stats } = UserData;
+  // const { title, stats } = UserStat;
+  const { title, stats: statData } = UserStat;
+
+
+    return (
     <div
       style={{
         height: '100vh',
@@ -10,7 +25,12 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+          stats={stats} />
+        <Statistics title={title} stats={statData} />
     </div>
   );
 };
